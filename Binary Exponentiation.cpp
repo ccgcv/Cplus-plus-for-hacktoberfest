4a04dda9b4adb0204a135bf753832c121e9c9b6c
attttt17 <57448162+attttt17@users.noreply.github.com>
2022-10-02 21:49:23 +0530

    Add files via upload

---
+++ Binary Exponentiation.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+int power(long long x, unsigned int y, int p)
+{
+    int res = 1;     
+ 
+    x = x % p; 
+  
+    if (x == 0) return 0; 
+ 
+    while (y > 0)
+    {
+        
+        if (y & 1)
+            res = (res*x) % p;
+ 
+        y = y>>1; // y = y/2
+        x = (x*x) % p;
+    }
+    return res;
+}
+ 
+
+int main()
+{
+    int x = 2;
+    int y = 5;
+    int p = 13;
+    cout << "Power is " << power(x, y, p);
+    return 0;
+}
