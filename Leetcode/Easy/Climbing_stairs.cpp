459b2c2c51da0ec6ff2b52028502d3be6193c87b
Herculean17 <76151703+jinang17@users.noreply.github.com>
2022-10-04 12:25:57 +0530

    Create Climbing_stairs.cpp

---
+++ Leetcode/Easy/Climbing_stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+    vector<int> dp(n+1);
+        int prev=1;
+        int now=1;
+        int ans=0;
+        if(n==1) return 1;
+        
+        //cout<<dp[0]<<" "<<dp[1]<<" ";
+        for (int i=2; i<n+1 ; i++)
+        {
+            ans=now+prev;
+            prev=now;
+            now=ans;
+         //   cout<<dp[i]<<" ";
+        }
+       return ans;
+    }
+};
