19efc090f124191cb83a9b5b852057ba1705742c
Nikhil Kumar Das <72371791+NikhilKD@users.noreply.github.com>
2022-10-08 11:00:25 +0530

    Create Printing_Longest_Increasing_Subsequence.cpp

---
+++ Printing_Longest_Increasing_Subsequence.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#include<unordered_set>
+
+using namespace std;
+
+int solve(string str) {
+
+  if(str.size()==0)
+      return 0;
+  int maxans = INT_MIN;
+  unordered_set < int > set;
+  int l = 0;
+  for (int r = 0; r < str.length(); r++) // outer loop for traversing the string
+  {
+    if (set.find(str[r]) != set.end()) //if duplicate element is found
+    {
+      while (l < r && set.find(str[r]) != set.end()) {
+        set.erase(str[l]);
+        l++;
+      }
+    }
+    set.insert(str[r]);
+    maxans = max(maxans, r - l + 1);
+  }
+  return maxans;
+}
+
+int main() {
+  string str = "takeUforward";
+  cout << "The length of the longest substring without repeating characters is " << 
+  solve(str);
+  return 0;
+}
